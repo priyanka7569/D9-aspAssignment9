9b138815b9b591219e79705b9f0d3b6bf1c1ba6c
priyanka7569 <149995767+priyanka7569@users.noreply.github.com>
2023-12-20 15:57:48 +0530

    Update Customer.aspx.cs

--- Assignment 9 Customer WebForm/Customer.aspx.cs
+++ Assignment 9 Customer WebForm/Customer.aspx.cs
@@ -5,18 +5,29 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Assignment_9_Customer_WebForm
+namespace Assessment_9_Web_Form_Customer
 {
     public partial class Customer : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Page.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            LblInfo.Visible = false;
         }
 
         protected void BtnRegister_Click(object sender, EventArgs e)
         {
+            LblInfo.Visible=true;
+            LblInfo.Text += "Welcome " + TxtCustName.Text;
+            LblInfo.Text += "Your Mobile Number: " + TxtCustMob.Text;
+            LblInfo.Text += "Your Email: " + TxtCustEmail.Text;
+            LblInfo.Text += "Your Password: " + TxtCustPassword.Text;
+        }
 
+        protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            string name = args.Value;
+            args.IsValid = name.Length == 6;
         }
     }
 }
